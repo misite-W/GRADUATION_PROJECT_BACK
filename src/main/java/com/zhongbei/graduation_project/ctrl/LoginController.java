4d0708b6df670b8b6e5ccdcef7c114a7088d0788
wangzikun <18406587187@163.com>
2020-01-16 14:55:53 +0800

    毕设

--- src/main/java/com/zhongbei/graduation_project/ctrl/LoginController.java
+++ src/main/java/com/zhongbei/graduation_project/ctrl/LoginController.java
@@ -1,12 +1,10 @@
 package com.zhongbei.graduation_project.ctrl;
-
 import com.zhongbei.graduation_project.entity.Users;
 import com.zhongbei.graduation_project.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
